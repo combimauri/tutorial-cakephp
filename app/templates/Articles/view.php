0b7fd24a3e1c97b68cdec0b1712dc9507a4ae015
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2023-11-06 18:21:54 -0300

    Update articles views to include tags

--- app/templates/Articles/view.php
+++ app/templates/Articles/view.php
@@ -10,6 +10,9 @@
 <p>
     <?= h($article->body) ?>
 </p>
+<p><b>Tags:</b>
+    <?= h($article->tag_string) ?>
+</p>
 <p><small>Created:
         <?= $article->created->format(DATE_RFC850) ?>
     </small></p>
