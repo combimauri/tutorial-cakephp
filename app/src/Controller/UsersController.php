ebfe7b95d0a936afae8c9ca2b8d3f1d622d543c5
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2023-11-06 19:37:54 -0300

    Add login and logout methods

--- app/src/Controller/UsersController.php
+++ app/src/Controller/UsersController.php
@@ -7,6 +7,7 @@ namespace App\Controller;
  * Users Controller
  *
  * @property \App\Model\Table\UsersTable $Users
+ * @property \Authentication\Controller\Component\AuthenticationComponent $Authentication
  * @method \App\Model\Entity\User[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
  */
 class UsersController extends AppController
@@ -21,6 +22,65 @@ class UsersController extends AppController
         $users = $this->paginate($this->Users);
 
         $this->set(compact('users'));
+    }
+
+    /**
+     * Before filter method.
+     *
+     * @param \Cake\Event\EventInterface $event The beforeFilter event.
+     * @return void
+     */
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        // Configure the login action to not require authentication, preventing
+        // the infinite redirect loop issue
+        $this->Authentication->addUnauthenticatedActions(['login', 'add']);
+    }
+
+    /**
+     * Login method.
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function login(): ?\Cake\Http\Response
+    {
+        $this->request->allowMethod(['get', 'post']);
+        $result = $this->Authentication->getResult();
+
+        if ($result !== null && $result->isValid()) {
+            // redirect to /articles after login success
+            $redirect = $this->request->getQuery('redirect') ?? [
+                'controller' => 'Articles',
+                'action' => 'index',
+            ];
+
+            return $this->redirect($redirect);
+        }
+        // display error if user submitted and authentication failed
+        if ($this->request->is('post') && ($result === null || !$result->isValid())) {
+            $this->Flash->error(__('Invalid username or password'));
+        }
+
+        return null;
+    }
+
+    /**
+     * Logout method.
+     *
+     * @return \Cake\Http\Response|null
+     */
+    public function logout(): ?\Cake\Http\Response
+    {
+        $result = $this->Authentication->getResult();
+
+        if ($result !== null && $result->isValid()) {
+            $this->Authentication->logout();
+
+            return $this->redirect(['controller' => 'Users', 'action' => 'login']);
+        }
+
+        return null;
     }
 
     /**
