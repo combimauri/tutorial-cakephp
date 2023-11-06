8d945ea5b7decf4f563a5312b348c1dfafe04b32
Mauricio Arce Torrez <mauricio.arce.mat@gmail.com>
2023-11-06 16:15:01 -0300

    Add logic to generate slug and validate data before saving

--- app/src/Model/Table/ArticlesTable.php
+++ app/src/Model/Table/ArticlesTable.php
@@ -1,10 +1,14 @@
 <?php
 declare(strict_types=1);
 
-// src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
+use ArrayObject;
+use Cake\Event\EventInterface;
+use Cake\ORM\Entity;
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Validation\Validator;
 
 class ArticlesTable extends Table
 {
@@ -17,5 +21,41 @@ class ArticlesTable extends Table
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    /**
+     * Add slug with beforeSave event.
+     *
+     * @param \Cake\Event\EventInterface $event beforeSave event
+     * @param \App\Model\Entity\Article $entity initial beforeSave entity
+     * @param \ArrayObject $options beforeSave options
+     * @return void
+     */
+    public function beforeSave(EventInterface $event, Entity $entity, ArrayObject $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $sluggedTitle = Text::slug($entity->title);
+            // trim slug to maximum length defined in schema
+            $entity->slug = substr($sluggedTitle, 0, 191);
+        }
+    }
+
+    /**
+     * Validate Article.
+     *
+     * @param \Cake\Validation\Validator $validator The validator object
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator): Validator
+    {
+        $validator
+            ->notEmptyString('title')
+            ->minLength('title', 10)
+            ->maxLength('title', 255)
+
+            ->notEmptyString('body')
+            ->minLength('body', 10);
+
+        return $validator;
     }
 }
